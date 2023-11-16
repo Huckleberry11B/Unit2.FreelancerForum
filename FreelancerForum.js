85caad1eea0bc5b486a2886d1bceb7fd30aa5725
Huckleberry11B <huckleberry@HuckleBerry11B>
2023-11-15 20:30:13 -0600

    first push and all i got is the array of objects...

---
+++ FreelancerForum.js
@@ -0,0 +1,26 @@
+const freelancers = [
+    {name: "Dr. Doom", price: 50, occupation: "Evil Genius"},
+    {name: "Dr. Pepper", price: 3.50, occupation: "Soul Soother"},
+    {name: "The Doctor", price: 100, occupation: "Time Traveler"},
+    {name: "James Earl Jones", price: 200, occupation: "Darth Vader"},
+    {name: "Ryan Renalds", price: 69, occupation: "Dead Pool"},
+    {name: "Wolverine", price: 75, occupation: "Anti-Hero"},
+    {name: "Kratos", price: 14, occupation: "God Killer"},
+];
+
+
+
+// const price = 0;
+
+// function averagePrice() {
+//     for (let i = 0; i < freelancers.length; i++) {
+//         const price = freelancers[i][1];
+    
+//         let avg = (price/freelancers.length);
+                
+//     }
+//     console.log(avg);
+// }
+
+// const avg = freelancers => freelancers.reduce((a, b) => a + b) / freelancers.length;
+// console.log(avg);
